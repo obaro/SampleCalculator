4443a332198e71bf402103490669348a298a9f44
Md Nazrul Islam <md.nazrul.islam.7.1999@gmail.com>
2021-11-16 22:51:41 +0600

    Lab.java file has been added to the master

---
+++ Lab.java
@@ -0,0 +1,11 @@
+public class Lab {
+    private int number;
+ 
+    public int getNumber() {
+        return this.number;
+    }
+ 
+    public void setNumber(int num) {
+        this.number = num;
+    }
+}
